178855f70ed4ba8fe5ecdeee3364857a32d66b19
Minio931 <d.chmielowski01@gmail.com>
2022-12-01 13:07:47 +0100

    Labolatorium 8 01-12-2022

---
+++ Services/PersonService.cs
@@ -0,0 +1,31 @@
+﻿using MVC_LAB.Context;
+using MVC_LAB.Models.Person;
+using NetTopologySuite.Index.Strtree;
+
+namespace MVC_LAB.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly PersonContext _context;
+        public PersonService(PersonContext context)
+        {
+            _context = context;
+        }
+        
+        public List<PersonModel> GetPersons()
+        {
+            return _context.Persons.ToList();
+        }
+        public void CreatePerson(int id, string name, string city, GenderEnum gender)
+        {
+            _context.Add(new PersonModel()
+            {
+                ID = id,
+                Name = name,
+                City = city,
+                Gender = gender
+            });
+            _context.SaveChanges();
+        }
+    }
+}
